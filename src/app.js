aedf93d02c5fa8b635e4f961a7538d2ba637e540
Diego Beker <diegobecker2007@hotmail.com>
2023-04-16 14:07:49 -0300

    Add endpoint POST /participants

--- src/app.js
+++ src/app.js
@@ -1,10 +1,100 @@
 import express from "express";
 import cors from "cors";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+import dayjs from "dayjs";
+import joi from "joi"
 
 const app = express();
+
+//config
 app.use(cors());
 app.use(express.json());
+dotenv.config();
 
+//MongoDB
+let db;
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+mongoClient
+  .connect()
+  .then(() => (db = mongoClient.db()))
+  .catch((err) => console.log(err.message));
+
+app.post("/participants", async (req, res) => {
+  
+  const userSchema = joi.object({
+    name: joi.string().required()
+  });
+  const validation = userSchema.validate(req.body, {abortEarly: false});
+
+  if (validation.error) {
+    const errors = validation.error.details.map((detail) => detail.message);
+    return res.status(422).send(errors);
+  }
+
+  try {
+    const participantExists = await db
+      .collection("participants")
+      .findOne({ name: req.body.name });
+
+    if (participantExists) return res.sendStatus(409);
+
+    await db
+      .collection("participants")
+      .insertOne({ name: req.body.name, lastStatus: Date.now() });
+
+    return res.sendStatus(201);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.get("/participants", async (req, res) => {
+  try {
+    const participants = await db.collection("participants").find().toArray();
+    res.send(participants);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.post("/messages", async (req, res) => {
+  const { to, text, type } = req.body;
+  const user = req.headers.user;
+  const time = dayjs().format("HH:mm:ss");
+
+  try {
+    await db
+      .collection("messages")
+      .insertOne({ from: user, to: to, text: text, type: type, time: time });
+    res.sendStatus(201);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.get("/messages", async (req, res) => {
+  const user = req.headers.user;
+  const limit = req.query.limit;
+
+  try {
+    if (limit) {
+      console.log(limit);
+      const messages = await db
+        .collection("messages")
+        .find({ $or: [{ to: user }, { from: user }, { to: "Todos" }] })
+        .toArray();
+      return res.send(messages.slice(-limit));
+    }
+    const messages = await db
+      .collection("messages")
+      .find({ $or: [{ to: user }, { from: user }, { to: "Todos" }] })
+      .toArray();
+    res.send(messages);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
