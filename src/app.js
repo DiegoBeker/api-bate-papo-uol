ad570c6086616780b324289047d660d622c970bb
Diego Beker <diegobecker2007@hotmail.com>
2023-04-16 14:46:59 -0300

    Add GET /participants e POST /messages

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ app.post("/participants", async (req, res) => {
 
   if (validation.error) {
     const errors = validation.error.details.map((detail) => detail.message);
-    return res.status(422).send(errors);
+    return res.sendStatus(422);
   }
 
   try {
@@ -43,15 +43,13 @@ app.post("/participants", async (req, res) => {
       .collection("participants")
       .insertOne({ name: req.body.name, lastStatus: Date.now() });
 
-    await db
-      .collection("messages")
-      .insertOne({
-        from: req.body.name,
-        to: "Todos",
-        text: "entra na sala...",
-        type: "status",
-        time: time,
-      });
+    await db.collection("messages").insertOne({
+      from: req.body.name,
+      to: "Todos",
+      text: "entra na sala...",
+      type: "status",
+      time: time,
+    });
 
     return res.sendStatus(201);
   } catch (error) {
@@ -69,14 +67,35 @@ app.get("/participants", async (req, res) => {
 });
 
 app.post("/messages", async (req, res) => {
-  const { to, text, type } = req.body;
+  const message = req.body;
   const user = req.headers.user;
   const time = dayjs().format("HH:mm:ss");
+  const messageSchema = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.valid("message", "private_message"),
+  });
+  const validation = messageSchema.validate(message, { abortEarly: false });
+
+  if (validation.error) {
+    const errors = validation.error.details.map((detail) => detail.message);
+    return res.sendStatus(422);
+  }
 
   try {
-    await db
-      .collection("messages")
-      .insertOne({ from: user, to: to, text: text, type: type, time: time });
+    const participantExists = await db
+      .collection("participants")
+      .findOne({ name: user });
+
+    if (!participantExists) return res.sendStatus(422);
+
+    await db.collection("messages").insertOne({
+      from: user,
+      to: message.to,
+      text: message.text,
+      type: message.type,
+      time: time,
+    });
     res.sendStatus(201);
   } catch (error) {
     res.status(500).send(error.message);
